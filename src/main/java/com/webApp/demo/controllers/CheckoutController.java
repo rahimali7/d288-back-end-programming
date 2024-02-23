e1a014c0164c1a2ce3c7477534d51eed919d4606
Admin <aali271@wgu.edu>
2024-02-23 11:40:05 -0800

    Wrote code for the Controllers package

---
+++ src/main/java/com/webApp/demo/controllers/CheckoutController.java
@@ -0,0 +1,29 @@
+package com.webApp.demo.controllers;
+
+import com.webApp.demo.dao.dto.Purchase;
+import com.webApp.demo.dao.dto.PurchaseResponse;
+import com.webApp.demo.services.CheckoutService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase){
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+
+}
